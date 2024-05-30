5d8a78ccddc98f9a2c7ac5ca7b890ebd89f68471
Marco <marcosf1625@gmail.com>
2024-05-30 11:28:35 -0300

    Update db.py

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ import sqlite3
 def inicailizarBD():
     conexao = sqlite3.connect('RadCadastro.db')
     cur = conexao.cursor()
+
     create = ["""
     CREATE TABLE IF NOT EXISTS Aluno 
         ( 
@@ -27,18 +28,24 @@ def inicailizarBD():
         );
     """]
 
-    fk_create = ['ALTER TABLE  Aluno ADD idCurso REFERENCES Curso (cursoID) ;',
-                 'ALTER TABLE  Aluno ADD idDisciplina REFERENCES Disciplina (disciplinaID) ;',
-                 'ALTER TABLE  Professores ADD idCurso REFERENCES Curso (cursoID) ;',
-                 'ALTER TABLE  Professores ADD idDisciplina REFERENCES Disciplina (disciplinaID) ;',
-                 'ALTER TABLE  Disciplina ADD idCurso REFERENCES Curso(cursoID);'
-    ]
     for i in create:
         cur.execute(i)
         conexao.commit()
-    for i in fk_create:
-        cur.execute(i)
-        conexao.commit()
+    fk_create(cur=cur , conexao=conexao)
+    cur.close()
+    conexao.close()
 
+def fk_create(cur , conexao):
 
+    try:
+        fk_create = ['ALTER TABLE  Aluno  ADD idCurso REFERENCES Curso (cursoID) ;',
+                     'ALTER TABLE  Aluno  ADD idDisciplina REFERENCES Disciplina (disciplinaID) ;',
+                     'ALTER TABLE  Professores  ADD idCurso REFERENCES Curso (cursoID) ;',
+                     'ALTER TABLE  Professores  ADD idDisciplina REFERENCES Disciplina (disciplinaID) ;',
+                     'ALTER TABLE  Disciplina  ADD idCurso REFERENCES Curso(cursoID);']
 
+        for i in fk_create:
+            cur.execute(i)
+            conexao.commit()
+    except sqlite3.OperationalError as e:
+        pass
